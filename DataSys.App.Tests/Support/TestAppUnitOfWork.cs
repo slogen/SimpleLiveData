cee2841f2681ceeac16bf2b882f6aec91bbe082d
Helge Jensen <hej@scadaminds.com>
2018-05-04 09:02:53 +0200

    Test DbContexts are now separated through TestAppUnitOfWorkFactory using an Id.

--- DataSys.App.Tests/Support/TestAppUnitOfWork.cs
+++ DataSys.App.Tests/Support/TestAppUnitOfWork.cs
@@ -1,9 +1,16 @@
-﻿using DataSys.App.DataAccess;
+﻿using System.Threading;
+using DataSys.App.DataAccess;
 
 namespace DataSys.App.Tests.Support
 {
     public class TestAppUnitOfWorkFactory: AppUnitOfWorkFactory
     {
-        protected override string DbContextName => "TestAppDb";
+        private static long _id;
+        public long Id { get; }
+        public TestAppUnitOfWorkFactory()
+        {
+            Id = Interlocked.Increment(ref _id);
+        }
+        protected override string DbContextName => $"TestAppDb{Id}";
     }
 }
