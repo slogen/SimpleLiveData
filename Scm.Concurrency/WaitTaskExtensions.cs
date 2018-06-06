47698c25c836a482e96cf8b9b2f67bab92a29065
Helge Jensen <hej@scadaminds.com>
2018-06-06 07:18:28 +0200

    Task extension WaitAsync

---
+++ Scm.Concurrency/WaitTaskExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scm.Concurrency
+{
+    public static class WaitTaskExtensions
+    {
+        public static async Task WaitAsync(this Task task, CancellationToken cancellationToken)
+        {
+            await (await Task.WhenAny(task, Task.Delay(-1, cancellationToken)).ConfigureAwait(false)).ConfigureAwait(false);
+        }
+    }
+}
