50c241efa3db24415ce8e066e363b80f0225ab1f
Helge Jensen <hej@scadaminds.com>
2018-06-06 07:27:21 +0200

    Observer for release of SemaphoreSlim

---
+++ Scm.Rx/ObserveSemaphoreSlim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace Scm.Rx
+{
+    public static class ObserveSemaphoreSlim
+    {
+        public static IObservable<long> ObserveRelease(this SemaphoreSlim semaphore)
+        {
+            return Observable.Create<long>(async (obs, ct) => {
+                var i = 0L;
+                while (!ct.IsCancellationRequested)
+                {
+                    await semaphore.WaitAsync(ct).ConfigureAwait(false);
+                    obs.OnNext(i++);
+                }
+            });
+        }
+    }
+
+
+}
