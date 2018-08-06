7fc1fbe2f3e98fc1397daecc483d95501868cdcc
Helge Jensen <hej@scadaminds.com>
2018-08-06 11:37:42 +0200

    Helper for Dispose IEnumerable<T> where T: IDisposable

---
+++ Scm.Sys/AggregateDisposeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scm.Sys
+{
+    public static class AggregateDisposeExtensions
+    {
+        public static void DisposeAll<T>(this IEnumerable<T> disposables) where T: IDisposable
+        {
+            var exceptions = new List<Exception>();
+            foreach(var d in disposables) 
+                try
+                {
+                    d.Dispose();
+                } catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            if (exceptions.Any())
+                throw new AggregateException(exceptions);
+        }
+    }
+}
