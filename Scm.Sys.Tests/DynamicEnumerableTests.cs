dd53c2c1a47053c759895631385ab910d100c9f4
Helge Jensen <hej@scadaminds.com>
2018-08-08 12:34:42 +0200

    Proper test-coverage of DynamicEnumerable

---
+++ Scm.Sys.Tests/DynamicEnumerableTests.cs
@@ -0,0 +1,56 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Xunit;
+
+namespace Scm.Sys.Tests
+{
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration", Justification = "These tests execise exactly that")]
+    public class DynamicEnumerableTests
+    {
+        [Fact]
+        public void DynamicEnumerable_Elementtype_ThrowsOnNullElementType()
+        {
+            default(IEnumerable).Invoking(x => x.ElementType())
+                .Should().Throw<ArgumentNullException>().Which.Message.Should().Contain("enumerable");
+        }
+        [Fact]
+        public void DynamicEnumerable_Elementtype_ThrowsOnMissingIEnumerableTImplementation()
+        {
+            var e = A.Fake<IEnumerable>();
+            e.Invoking(x => x.ElementType())
+                .Should().Throw<DynamicEnumerable.InconclusiveElementTypeException>()
+                .Which.Should().BeEquivalentTo(new
+                {
+                    SourceObject = e,
+                    CandidateTypes = new Type[0]
+                });
+        }
+
+
+        [SuppressMessage("ReSharper", "PossibleInterfaceMemberAmbiguity", Justification = "Tests for exactly that")]
+        public interface IMultipleIEnumerableImplementations: IEnumerable<int>, IEnumerable<float> { }
+        [Fact]
+        public void DynamicEnumerable_Elementtype_ThrowsOnMultipleIEnumerableTImplementation()
+        {
+            var e = A.Fake<IMultipleIEnumerableImplementations>();
+            e.Invoking(x => x.ElementType())
+                .Should().Throw<DynamicEnumerable.InconclusiveElementTypeException>()
+                .Which.Should().BeEquivalentTo(new
+                {
+                    SourceObject = e,
+                    CandidateTypes = typeof(IMultipleIEnumerableImplementations).GetInterfaces().Where(x => x != typeof(IEnumerable))
+                });
+        }
+        [Fact]
+        public void DynamicEnumerable_Elementtype_ExtractsElementType()
+        {
+            A.Fake<IEnumerable<float>>().ElementType().Should().Be(typeof(float));
+        }
+
+    }
+}
--- Scm.Sys/DynamicEnumerable.cs
+++ Scm.Sys/DynamicEnumerable.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Scm.Sys
 {
     public static class DynamicEnumerable
     {
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public class InconclusiveElementTypeException : ArgumentException
+        {
+            public object SourceObject { get; }
+            public IEnumerable<Type> CandidateTypes { get; }
+
+            public InconclusiveElementTypeException(object sourceObject, IEnumerable<Type> candidateTypes, string argument)
+            :base(MakeMessage(sourceObject, candidateTypes, argument), argument)
+            {
+                SourceObject = sourceObject;
+                CandidateTypes = candidateTypes;
+            }
+
+            protected static string MakeMessage(object source, IEnumerable<Type> candidateTypes, string argument)
+            {
+                if (candidateTypes.Skip(1).Any())
+                    return
+                        $"Unable to deduce elementtype from {source}. It implements IEnumerable<T> multiples times"
+                        + $"for: {string.Join(", ", candidateTypes.Select(t => $"{t.GetGenericArguments().Single()}"))}";
+                return $"Unable to deduce elementtype from {source}";
+            }
+        }
         public static Type ElementType(this IEnumerable enumerable)
         {
             if (enumerable == null)
@@ -18,12 +41,8 @@ namespace Scm.Sys
                 var tg = t.GetGenericTypeDefinition();
                 return tg == typeof(IEnumerable<>);
             }).ToList();
-            if (!enumTypes.Any())
-                throw new ArgumentException($"Unable to deduce elementtype from a {enumerable.GetType()}");
-            if (enumTypes.Count > 1)
-                throw new ArgumentException(
-                    $"Unable to deduce elementtype from a {enumerable.GetType()}. It implements IEnumerable<T> {enumTypes.Count} times"
-                    + $"for: {string.Join(", ", enumTypes.Select(t => $"{t.GetGenericArguments().Single()}"))}");
+            if (enumTypes.Count != 1)
+                throw new InconclusiveElementTypeException(enumerable, enumTypes, nameof(enumerable));
             return enumTypes.Single().GenericArguments().Single();
         }
     }
