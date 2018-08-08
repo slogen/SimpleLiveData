1bd4bdf144c5d82129f85b6662a13352847ea1b9
Helge Jensen <hej@scadaminds.com>
2018-08-08 11:49:04 +0200

    Split out dynamic helpers for Enumerable and Generic

---
+++ Scm.Sys/DynamicEnumerable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scm.Sys
+{
+    public static class DynamicEnumerable
+    {
+        public static Type ElementType(this IEnumerable enumerable)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+            var enumTypes = enumerable.GetType().GetInterfaces().Where(t =>
+            {
+                if (!t.IsGenericType)
+                    return false;
+                var tg = t.GetGenericTypeDefinition();
+                return tg == typeof(IEnumerable<>);
+            }).ToList();
+            if (!enumTypes.Any())
+                throw new ArgumentException($"Unable to deduce elementtype from a {enumerable.GetType()}");
+            if (enumTypes.Count > 1)
+                throw new ArgumentException(
+                    $"Unable to deduce elementtype from a {enumerable.GetType()}. It implements IEnumerable<T> {enumTypes.Count} times"
+                    + $"for: {string.Join(", ", enumTypes.Select(t => $"{t.GetGenericArguments().Single()}"))}");
+            return enumTypes.Single().GenericArguments().Single();
+        }
+    }
+}
---
+++ Scm.Sys/DynamicGeneric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scm.Sys
+{
+    public static class DynamicGeneric
+    {
+        public static IEnumerable<Type> GenericArguments(this Type t)
+        {
+            if (t == null)
+                throw new ArgumentNullException(nameof(t));
+            if (!t.IsGenericType)
+                return Enumerable.Empty<Type>();
+            return t.GetGenericArguments();
+        }
+    }
+}
