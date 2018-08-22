5d0fd6f007bb88fbbbb3a9c5f5e75a8ff9b4f7eb
Helge Jensen <hej@scadaminds.com>
2018-08-22 12:21:09 +0200

    TraceHere refactor for additional flexibility

---
+++ Scm.Rx/Trace/ActionHereTracer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Scm.Sys;
+
+namespace Scm.Rx.Trace
+{
+    public class ActionHereTracer : AbstractHereTracer
+    {
+        public Action<string, object[]> Action { get; set; }
+        public ActionHereTracer(Action<string, object[]> action, ICallerInfo callerInfo, bool? enabled = null) : base(callerInfo, enabled)
+        {
+            Action = action;
+        }
+
+        protected override void WriteLine(string format, params object[] args)
+            => Action?.Invoke(format, args);
+    }
+}
--- Scm.Rx/TraceExtensions.cs
+++ Scm.Rx/TraceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
@@ -9,31 +10,39 @@ namespace Scm.Rx
 {
     public static class TraceExtensions
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IObservable<T> Trace<T>(
-            this IObservable<T> source,
-            TextWriter writer,
-            ICallerInfo callerInfo,
-            bool? enabled = null)
-            => source.Wrap(new TextWriterHereTracer(writer, callerInfo, enabled).Trace);
+        public static Func<IObservable<T>, IObservable<T>> DefaultWrapper<T>(ICallerInfo callerInfo, bool? enabled = null)
+        {
+            Func<IObservable<T>, IObservable<T>> wrap = o => o;
+            if (Debugger.IsLogging())
+            {
+                var wrap1 = wrap;
+                wrap = o => new ActionHereTracer((f, a) => Debug.WriteLine(f, a), callerInfo, enabled).Trace(wrap1(o));
+            }
+
+            {
+                var wrap1 = wrap;
+                wrap = o => new TextWriterHereTracer(Console.Error, callerInfo, enabled).Trace(wrap1(o));
+            }
+            return wrap;
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [SuppressMessage("ReSharper", "ExplicitCallerInfoArgument", Justification = "Pass callerinfo to target")]
         public static IObservable<T> TraceHere<T>(
             this IObservable<T> source,
-            TextWriter writer = null,
             bool? enabled = null,
+            Func<ICallerInfo, bool?, Func<IObservable<T>, IObservable<T>>> target = null,
             [CallerMemberName] string callerMemberName = null,
             [CallerFilePath] string callerFilePath = null,
             [CallerLineNumber] int callerLineNumber = 0)
-            => source.Trace(writer ?? Console.Error,
-                CallerInfo.Here(callerMemberName, callerFilePath, callerLineNumber), enabled);
+        {
+            var here = CallerInfo.Here(callerMemberName, callerFilePath, callerLineNumber);
+            return source.Wrap(target == null ? DefaultWrapper<T>(here, enabled) : target(here, enabled));
+        }
 
         public static IObservable<T> Wrap<T>(
             this IObservable<T> source,
-            Func<IObservable<T>, IObservable<T>> wrapper)
-        {
-            return wrapper == null ? source : wrapper(source);
-        }
+            Func<IObservable<T>, IObservable<T>> wrapper) 
+            => wrapper == null ? source : wrapper(source);
     }
 }
